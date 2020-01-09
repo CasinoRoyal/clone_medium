cb21a1cd4b0373a90419833950a9de85d93cd0e5
CasinoRoyal <lux_ex_tenebris@outlook.com>
2020-01-09 20:14:39 +0200

    Created user context

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from 'routes';
 import Navbar from 'components/navbar';
+import { CurrentUserProvider } from 'contexts/current-user';
 
 const App = () => {
   return(
-    <Router>
-      <Navbar />
-      <Routes />      
-    </Router>
+    <CurrentUserProvider>
+      <Router>
+        <Navbar />
+        <Routes />      
+      </Router>
+    </CurrentUserProvider>
   );
 };
 
